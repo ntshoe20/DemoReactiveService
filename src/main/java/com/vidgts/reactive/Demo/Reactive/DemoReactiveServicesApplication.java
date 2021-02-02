c29cb510dc87ecbb9c172b6eb27cd774753d73dc
ntshoe20 <itumeleng.ntshoe@gmail.com>
2021-02-03 00:14:36 +0200

    new structure

---
+++ src/main/java/com/vidgts/reactive/Demo/Reactive/DemoReactiveServicesApplication.java
@@ -0,0 +1,13 @@
+package com.vidgts.reactive.Demo.Reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactiveServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactiveServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidgts/reactive/Demo/Reactive/DemoReactiveServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidgts.reactive.Demo.Reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactiveServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
